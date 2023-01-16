bd1f7d47b4cec579fd04a8780ab57e911eb599f8
“vivek-221 <chagarlamudi.vivek@gmail.com>
2023-01-16 15:20:52 -0500

    Added the basic code

---
+++ src/test/java/com/project/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
